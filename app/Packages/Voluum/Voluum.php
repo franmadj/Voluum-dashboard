aec6edf6df9c03df94983c13968a61f01449d3f6
Francisco Mauri <labrest03@gmail.com>
2020-11-10 20:47:20 +0100

    clean up some comments

--- app/Packages/Voluum/Voluum.php
+++ app/Packages/Voluum/Voluum.php
@@ -20,13 +20,11 @@ class Voluum {
 
     public function get_dashboard_data($dateRange) {
         $dates = $this->get_date_ranges($dateRange);
-
         foreach ($this->accounts_data as $key => $acc) {
             $this->auth_token = $acc['token'];
             $data = $this->request_report($acc, $dates);
             $this->accounts_data[$key]['data'] = $data;
         }
-        //var_dump($this->accounts_data);
         return $this->accounts_data;
     }
 
@@ -39,21 +37,15 @@ class Voluum {
             $to = date('Y-m-d\T', strtotime($dateRange['date_to'] . ' +1 days')) . '00:00:00Z';
             $dates = '&from=' . urlencode($from) . '&to=' . urlencode($to);
         }
-        //var_dump($dates);
-
         return $dates .= '&tz=CET';
     }
 
     private function request_report($acc, $dates) {
-        //https://api.voluum.com/report?include=ALL&groupBy=affiliateNetworkId&from=2017-05-20T00%3A00%3A00Z&workspaces=21fbb9c8-9c57-4bff-9da8-ab1fa5c932a3
         $query = 'include=ALL&groupBy=affiliateNetworkId&conversionTimeMode=CONVERSION';
         $base_url = "https://api.voluum.com/report?";
 
         $url = $base_url . $query . $dates;
         $to_month_url = $base_url . $query;
-        //$url='https://api.voluum.com/report?include=ALL&groupBy=affiliateNetworkId&from=2017-05-20T00%3A00%3A00Z&to=2020-10-30T00%3A00%3A00Z';
-        //exit;
-        //$report['acc'] = $this->query_report($url);
         $report = [];
         if (!empty($acc['workspaces'])) {
             $report['ws'] = [];
@@ -75,9 +67,6 @@ class Voluum {
         $to = date('Y-m-d\T', strtotime(' +1 days')) . '00:00:00';
         $dates = '&from=' . urlencode($from) . '&to=' . urlencode($to);
         $to_month_url .= $dates . '&tz=CET';
-
-        //var_dump($to_month_url);
-
         if ($result = $this->query_report($to_month_url)) {
             return $result->profit;
         }
@@ -85,8 +74,6 @@ class Voluum {
     }
 
     private function query_report($url) {
-//        if ($cache = $this->get_cache($url))
-//            return $cache;
         try {
             $client = new Client();
             $response = $client->request("GET", $url, [
@@ -98,7 +85,6 @@ class Voluum {
             $code = $response->getStatusCode();
             if (200 == $code) {
                 $result = json_decode((string) $response->getBody())->totals;
-                //$this->set_cache($url, $result);
                 return $result;
             }
         } catch (ClientException $e) {
@@ -109,8 +95,6 @@ class Voluum {
 
     private function set_daterange_to_today($date_to) {
         $this->is_daterange_to_past = date('Y-m-d') > date('Y-m-d', strtotime($date_to));
-//        var_dump($this->is_daterange_to_past);
-//        exit;
     }
 
     private function set_cache($url, $new_data) {
@@ -204,75 +188,5 @@ class Voluum {
         return false;
     }
 
-    /* private function get_clicks_and_visits($rows) {
-      if (!$rows)
-      return;
-      $campaigns = [];
-      $clicks = $visits = $i = $n = 0;
-      foreach ($rows as $row) {
-      if (!isset($campaigns[$row->campaignId])) {
-      $i++;
-
-
-      $campaigns[$row->campaignId] = $row->campaignId;
-      $clicks += $this->request_clicks_and_visits($row->campaignId, 'clicks');
-      $visits += $this->request_clicks_and_visits($row->campaignId, 'visits');
-      if ($i >= 5) {
-      sleep(2);
-      $i = 0;
-      }
-      }
-      }
-      return['clicks' => $clicks, 'visits' => $visits];
-      }
-
-      private function request_clicks_and_visits($campaign, $type = 'clicks') {
-
-      //var_dump($campaign, $type);return 1;
-
-      $client = new Client();
-      $url = "https://api.voluum.com/report/live/$type/" . $campaign;
-      $response = $client->request("GET", $url, [
-      'headers' => [
-      'Accept' => 'application/json',
-      'CWAUTH-TOKEN' => $this->auth_token,
-      ]]);
-      $code = $response->getStatusCode();
-      if (200 == $code) {
-      $data = json_decode((string) $response->getBody());
-      return $data->totalRows;
-      }
-      return false;
-      }
-
-      private function request_report_old($acc, $dateRange) {
-      $columns = 'column=visits&column=clicks&column=conversions&column=revenue&column=cost&column=profit';
-      $workspaces = '';
-      $dates = '';
-      if (!empty($acc['workspaces'])) {
-      $ws = array_map('trim', explode(',', $acc['workspaces']));
-      $workspaces = '';
-      foreach ($ws as $_ws) {
-      $workspaces .= '&workspaces=' . $_ws;
-      }
-      }
-      $dates = '&from=2020-10-30T00:00:00Z';
-      if ($dateRange) {
-      $dates = '&from=' . $dateRange['date_from'] . '&to' . $dateRange['date_to'];
-      }
-      $query = $columns . $workspaces . $dates;
-      $client = new Client();
-      $url = "https://api.voluum.com/report/conversions?" . $query;
-      $response = $client->request("GET", $url, [
-      'headers' => [
-      'Content-Type' => 'application/json; charset=utf-8',
-      'Accept' => 'application/json',
-      'CWAUTH-TOKEN' => $acc['token'],
-      ]]);
-      $code = $response->getStatusCode();
-      if (200 == $code) {
-      return json_decode((string) $response->getBody());
-      }
-      return false;
-      } */
+    
 }
